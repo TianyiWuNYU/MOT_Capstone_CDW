1a3779b9659b8e8fdbb91ec67604a639ccc7178e
Tianyi Wu <166233984+TianyiWuNYU@users.noreply.github.com>
2024-04-09 13:28:50 -0400

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -32,7 +32,7 @@ def mapping_demo():
 
     try:
         ALL_LAYERS = {
-            "Facility Name": pdk.Layer(
+            "Generator": pdk.Layer(
                 "HexagonLayer",
                 data=from_data_file("bike_rental_stats.json"),
                 get_position=["lon", "lat"],
@@ -41,7 +41,7 @@ def mapping_demo():
                 elevation_range=[0, 1000],
                 extruded=True,
             ),
-            "Region": pdk.Layer(
+            "Receiver": pdk.Layer(
                 "ScatterplotLayer",
                 data=from_data_file("bart_stop_stats.json"),
                 get_position=["lon", "lat"],
@@ -49,7 +49,7 @@ def mapping_demo():
                 get_radius="[exits]",
                 radius_scale=0.05,
             ),
-             "Activity Number": pdk.Layer(
+             "Transporter": pdk.Layer(
                 "ScatterplotLayer",
                 data=from_data_file("bart_stop_stats.json"),
                 get_position=["lon", "lat"],
@@ -57,7 +57,7 @@ def mapping_demo():
                 get_radius="[exits]",
                 radius_scale=0.05,
             ),
-            "Utilized Capacity for reported year (cubic yards)": pdk.Layer(
+            "Waste Quantity": pdk.Layer(
                 "TextLayer",
                 data=from_data_file("bart_stop_stats.json"),
                 get_position=["lon", "lat"],
@@ -66,7 +66,7 @@ def mapping_demo():
                 get_size=10,
                 get_alignment_baseline="'bottom'",
             ),
-            "Remaining Constructed Capacity (cubic yards)": pdk.Layer(
+            "City": pdk.Layer(
                 "TextLayer",
                 data=from_data_file("bart_stop_stats.json"),
                 get_position=["lon", "lat"],
@@ -74,28 +74,6 @@ def mapping_demo():
                 get_color=[0, 0, 0, 200],
                 get_size=10,
                 get_alignment_baseline="'bottom'",
-            ),
-            "Year": pdk.Layer(
-                "TextLayer",
-                data=from_data_file("bart_stop_stats.json"),
-                get_position=["lon", "lat"],
-                get_text="name",
-                get_color=[0, 0, 0, 200],
-                get_size=10,
-                get_alignment_baseline="'bottom'",
-            ),
-            "Outbound Flow": pdk.Layer(
-                "ArcLayer",
-                data=from_data_file("bart_path_stats.json"),
-                get_source_position=["lon", "lat"],
-                get_target_position=["lon2", "lat2"],
-                get_source_color=[200, 30, 0, 160],
-                get_target_color=[200, 30, 0, 160],
-                auto_highlight=True,
-                width_scale=0.0001,
-                get_width="outbound",
-                width_min_pixels=3,
-                width_max_pixels=30,
             ),
         }
         st.sidebar.markdown("### Map Layers")
