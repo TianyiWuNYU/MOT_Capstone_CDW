c305b419c57f2cbf2907475b7c9f7bc2350a6ee8
Tianyi Wu <166233984+TianyiWuNYU@users.noreply.github.com>
2024-04-18 18:17:25 -0400

    Update File Upload.py

--- pages/File Upload.py
+++ pages/File Upload.py
@@ -33,13 +33,14 @@ with st.form("my_form"):
         city = selected_address['city'].iloc[0]
         state = selected_address['state'].iloc[0]
         zip = selected_address['zip'].iloc[0]
+        submitted = st.form_submit_button("Submit")
     else:
         name = ln
         address = add
         city = ct
         state = st
         zip = z
-    submitted = st.form_submit_button("Submit")
+        submitted = st.form_submit_button("Submit")
 
 if submitted:
     with open('data/test.csv', 'a', newline='', encoding='utf-8') as f:
