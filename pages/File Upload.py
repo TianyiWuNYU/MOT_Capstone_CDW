38a8ae9b39a4a86fc929bdbec590e8beb3d5814d
Tianyi Wu <166233984+TianyiWuNYU@users.noreply.github.com>
2024-04-18 20:32:05 -0400

    Update File Upload.py

--- pages/File Upload.py
+++ pages/File Upload.py
@@ -16,45 +16,46 @@ def load_test():
 def load_pickup_book():
     return pd.read_csv('data/pickup_address_book.csv')
 
-if 'selected_option' not in st.session_state:
-    st.session_state.selected_option = ""
-if 'selected_name' not in st.session_state:
-    st.session_state.selected_name = ""
-if 'selected_address' not in st.session_state:
-    st.session_state.selected_address = ""
-if 'selected_state' not in st.session_state:
-    st.session_state.selected_state = ""
-if 'selected_city' not in st.session_state:
-    st.session_state.selected_city = ""
-if 'selected_zip' not in st.session_state:
-    st.session_state.selected_zip = ""
+if 'pickup' not in st.session_state:
+    st.session_state.pickup = ""
+if 'pickup_name' not in st.session_state:
+    st.session_state.pickup_name = ""
+if 'pickup_add' not in st.session_state:
+    st.session_state.pickup_add = ""
+if 'pickup_state' not in st.session_state:
+    st.session_state.pickup_state = ""
+if 'pickup_city' not in st.session_state:
+    st.session_state.pickup_city = ""
+if 'pickup_zip' not in st.session_state:
+    st.session_state.pickup_zip = ""
 
 pickup_book = load_pickup_book()
 pickup_book['display'] = pickup_book['name'] + ' - ' + pickup_book['address']
-address_option = st.selectbox('Choose Existing Address', [''] + pickup_book['display'].tolist(), on_change=lambda: update_add_field(address_option), key="pickup")
+
+pickup_option = st.selectbox('Choose Existing Address', [''] + pickup_book['display'].tolist(), on_change=lambda: update_pickup(pickup_option), key="pickup")
 test_option = st.selectbox('Choose Existing Address', [''] + pickup_book['display'].tolist(),key="generator")
 
-def update_add_field(selected_option):
-    st.session_state.selected_option = selected_option
-    if selected_option:
-        selected_address = pickup_book[pickup_book['display'] == address_option]
-        st.session_state.selected_name = selected_address['name'].iloc[0]
-        st.session_state.selected_address = selected_address['address'].iloc[0]
-        st.session_state.selected_city = selected_address['city'].iloc[0]
-        st.session_state.selected_state = selected_address['state'].iloc[0]
-        st.session_state.selected_zip = selected_address['zip'].iloc[0]
+def update_pickup(option):
+    st.session_state.pickup = option
+    if option:
+        selected_pickup = pickup_book[pickup_book['display'] == pickup_option]
+        st.session_state.pickup_name = selected_pickup['name'].iloc[0]
+        st.session_state.pickup_add = selected_pickup['address'].iloc[0]
+        st.session_state.pickup_city = selected_pickup['city'].iloc[0]
+        st.session_state.pickup_state = selected_pickup['state'].iloc[0]
+        st.session_state.pickup_zip = selected_pickup['zip'].iloc[0]
         st.experimental_rerun()
 
-if address_option != st.session_state.selected_option:
-    update_add_field(address_option)
+if pickup_option != st.session_state.pickup:
+    update_pickup(pickup_option)
 
 with st.form("my_form"):
     st.write("### Pickup Location")
-    name = st.text_input("Location Name", value=st.session_state.selected_name)
-    address = st.text_input("Address", value=st.session_state.selected_address)
-    city = st.text_input("City", value=st.session_state.selected_city)
-    state = st.text_input("State", value=st.session_state.selected_state)
-    zip = st.text_input("Zip", value=st.session_state.selected_zip)
+    name = st.text_input("Location Name", value=st.session_state.pickup_name)
+    address = st.text_input("Address", value=st.session_state.pickup_add)
+    city = st.text_input("City", value=st.session_state.pickup_city)
+    state = st.text_input("State", value=st.session_state.pickup_state)
+    zip = st.text_input("Zip", value=st.session_state.pickup_zip)
     submitted = st.form_submit_button("Submit")
 
 if submitted:
