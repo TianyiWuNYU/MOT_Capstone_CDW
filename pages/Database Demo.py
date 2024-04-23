42a68633342d8866b7cbc25d14afc665a8aba192
bronzekillervan <126722429+bronzekillervan@users.noreply.github.com>
2024-04-22 20:18:08 -0400

    Update Database Demo.py

--- pages/Database Demo.py
+++ pages/Database Demo.py
@@ -1,21 +1,18 @@
 import streamlit as st
 import pandas as pd
 
+# 从GitHub加载数据
+@st.cache
 def load_data_from_github(url):
     return pd.read_csv(url)
 
+# GitHub上的CSV文件原始内容URL
 github_raw_url = 'https://raw.githubusercontent.com/TianyiWuNYU/test/main/data/cdw_csv_sample.csv'
-
 data = load_data_from_github(github_raw_url)
 
-if len(data.columns) > 1:
-    third_column_data = data.iloc[:, 9].to_frame()
-    third_column_data['index'] = third_column_data.index
-    st.dataframe(third_column_data)
-    
-    selected_indices = st.multiselect("Select an index", third_column_data['index'])
-    for selected_index in selected_indices:
-        st.write(data.loc[selected_index, :])
+# 选择栏
+selected_index = st.selectbox("Select an index", data.index)
 
-    st.write(data)
-
+# 使用expander来展示详细信息
+with st.expander("See details"):
+    st.write(data.loc[selected_index])
