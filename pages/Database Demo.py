6404062ba61c7ad61fe447e0e32a15582dfbc823
bronzekillervan <126722429+bronzekillervan@users.noreply.github.com>
2024-04-22 20:31:15 -0400

    Update Database Demo.py

--- pages/Database Demo.py
+++ pages/Database Demo.py
@@ -10,12 +10,16 @@ def load_data_from_github(url):
 github_raw_url = 'https://raw.githubusercontent.com/TianyiWuNYU/test/main/data/cdw_csv_sample.csv'
 data = load_data_from_github(github_raw_url)
 
-# 打印列名，以便我们可以看到所有可用的列名
-st.write("列名：", data.columns.tolist())
-
-# 假设第一列是index，第二列是generator_name，根据您的数据实际情况进行调整
-small_table = data.iloc[:, [0, 1]].copy()
-small_table.columns = ['index', 'generator_name']  # 重命名列为index和generator_name
+# 创建一个包含index和generator_name列的小表格
+small_table = data[['index', 'generator_name']].copy()
 
 # 展示小表格
+st.write("Index 和 Generator Name:")
 st.dataframe(small_table)
+
+# 允许用户选择一个index来查看详细信息
+selected_index = st.selectbox("Select an index to see details", data['index'])
+
+# 展示用户选中的详细信息
+st.write("Details at selected index:")
+st.write(data.loc[data['index'] == selected_index])
