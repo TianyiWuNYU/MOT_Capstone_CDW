2ef825d357368020719f006c50cddc290012d2b0
Tianyi Wu <166233984+TianyiWuNYU@users.noreply.github.com>
2024-04-23 14:11:02 -0400

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -39,7 +39,7 @@ def draw_routes(df):
         get_tilt=15,
         get_color=[255, 182, 193, 255], 
         pickable=True,
-        auto_highlight=True,
+        auto_highlight=False,
     )
 
     view_state = pdk.ViewState(
