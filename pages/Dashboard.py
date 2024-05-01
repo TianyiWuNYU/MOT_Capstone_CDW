157d68a6f3cd45bcbcc42e9e72dc475bdad5425f
bronzekillervan <126722429+bronzekillervan@users.noreply.github.com>
2024-05-01 14:42:34 -0400

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -2,31 +2,24 @@ import streamlit as st
 import pandas as pd
 import pydeck as pdk
 
-# 设置页面标题
 st.title('DDC Mapping Program')
 
-# 加载数据
 file_url = 'https://raw.githubusercontent.com/TianyiWuNYU/test/main/data/cdw_csv_processed.csv'
 df = pd.read_csv(file_url)
 
 st.write("Data loaded successfully!")
 
-# 用户界面允许选择type_debris，添加“All types”选项
 unique_debris_types = ['All types of debris'] + list(df['type_debris'].unique())
 selected_debris = st.selectbox('Select Type of Debris:', unique_debris_types)
 
-# 用户界面允许选择pickup_address，添加“All pickups”选项
 unique_pickup_addresses = ['All pickup addresses'] + list(df['pickup_address'].unique())
 selected_pickup_address = st.selectbox('Select Pickup Address:', unique_pickup_addresses)
 
-# 用户界面允许选择receiving_address，添加“All receiving addresses”选项
 unique_receiving_addresses = ['All receiving addresses'] + list(df['receiving_address'].unique())
 selected_receiving_address = st.selectbox('Select Receiving Address:', unique_receiving_addresses)
 
-# 让用户选择颜色
 route_color = st.color_picker('Choose a route color', '#87CEEB')  # 默认为天蓝色
 
-# 过滤数据逻辑
 filtered_data = df[
     ((df['type_debris'] == selected_debris) | (selected_debris == 'All types of debris')) &
     ((df['pickup_address'] == selected_pickup_address) | (selected_pickup_address == 'All pickup addresses')) &
@@ -49,7 +42,6 @@ def draw_routes(filtered_data, route_color):
             for _, row in filtered_data.iterrows()
         ]
 
-        # 解析用户选择的颜色
         color = [int(route_color.lstrip('#')[i:i+2], 16) for i in (0, 2, 4)] + [255]  # 将HEX颜色转换为RGBA
 
         layer = pdk.Layer(
@@ -59,7 +51,7 @@ def draw_routes(filtered_data, route_color):
             get_target_position="to_coordinates",
             get_width=5,
             get_tilt=15,
-            get_color=color,  # 使用用户选择的颜色
+            get_color=color,  
             pickable=True,
             auto_highlight=True,
         )
@@ -79,6 +71,5 @@ def draw_routes(filtered_data, route_color):
     else:
         st.error('No routes found for the selected options.')
 
-# 绘制路线图
 draw_routes(filtered_data, route_color)
 
