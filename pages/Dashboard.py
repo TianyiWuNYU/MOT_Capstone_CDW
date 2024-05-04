9c2c8f9c5b8e79014cccefdf95ffc36483b12dbb
bronzekillervan <126722429+bronzekillervan@users.noreply.github.com>
2024-05-04 14:54:36 -0400

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -22,7 +22,7 @@ pickup_color = st.color_picker('Choose a color for pickup addresses', '#FF6347')
 receiving_color = st.color_picker('Choose a color for receiving addresses', '#4682B4')
 
 def update_options():
-    """ 根据当前选择更新其它选项 """
+    """根据当前选择更新其它选项"""
     current_df = df.copy()
     if st.session_state.selected_debris != 'All types of debris':
         current_df = current_df[current_df['type_debris'] == st.session_state.selected_debris]
@@ -30,31 +30,26 @@ def update_options():
         current_df = current_df[current_df['pickup_address'] == st.session_state.selected_pickup_address]
     if st.session_state.selected_receiving_address != 'All receiving addresses':
         current_df = current_df[current_df['receiving_address'] == st.session_state.selected_receiving_address]
-    
-    debris_options = ['All types of debris'] + sorted(current_df['type_debris'].unique().tolist())
-    pickup_options = ['All pickup addresses'] + sorted(current_df['pickup_address'].unique().tolist())
-    receiving_options = ['All receiving addresses'] + sorted(current_df['receiving_address'].unique().tolist())
+
+    debris_types = current_df['type_debris'].unique().tolist()
+    if debris_types:  # 确保列表不为空
+        debris_options = ['All types of debris'] + sorted(debris_types)
+    else:
+        debris_options = ['All types of debris']  # 如果列表为空，则只有默认选项
+
+    pickup_addresses = current_df['pickup_address'].unique().tolist()
+    if pickup_addresses:
+        pickup_options = ['All pickup addresses'] + sorted(pickup_addresses)
+    else:
+        pickup_options = ['All pickup addresses']
+
+    receiving_addresses = current_df['receiving_address'].unique().tolist()
+    if receiving_addresses:
+        receiving_options = ['All receiving addresses'] + sorted(receiving_addresses)
+    else:
+        receiving_options = ['All receiving_addresses']
 
     return debris_options, pickup_options, receiving_options
-
-# 筛选器
-debris_options, pickup_options, receiving_options = update_options()
-
-selected_debris = st.selectbox('Select Type of Debris:', debris_options, index=debris_options.index(st.session_state.selected_debris), on_change=update_options)
-selected_pickup_address = st.selectbox('Select Pickup Address:', pickup_options, index=pickup_options.index(st.session_state.selected_pickup_address), on_change=update_options)
-selected_receiving_address = st.selectbox('Select Receiving Address:', receiving_options, index=receiving_options.index(st.session_state.selected_receiving_address), on_change=update_options)
-
-# 更新会话状态
-st.session_state.selected_debris = selected_debris
-st.session_state.selected_pickup_address = selected_pickup_address
-st.session_state.selected_receiving_address = selected_receiving_address
-
-# 过滤数据
-filtered_data = df[
-    ((df['type_debris'] == selected_debris) | (selected_debris == 'All types of debris')) &
-    ((df['pickup_address'] == selected_pickup_address) | (selected_pickup_address == 'All pickup addresses')) &
-    ((df['receiving_address'] == selected_receiving_address) | (selected_receiving_address == 'All receiving addresses'))
-]
 
 # 绘图函数，使用 Pydeck 绘制
 def draw_routes(filtered_data, pickup_color, receiving_color):
