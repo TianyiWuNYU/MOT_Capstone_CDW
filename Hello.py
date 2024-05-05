e74cc940be654588c742f5909c0c253619368eed
bronzekillervan <126722429+bronzekillervan@users.noreply.github.com>
2024-05-05 15:29:57 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,13 +4,15 @@ import pandas as pd
 def load_custom_css():
     css = """
     <style>
-        /* Target the grid layout of Streamlit columns directly */
+        /* Adjust the grid layout of Streamlit columns */
         .st-cd {
             gap: 20px; /* Adjust the gap between columns */
         }
         /* Adjustments for all images in the columns */
         .stImage img {
             width: 100%; /* Makes images responsive within the column width */
+            height: 250px; /* Uniform height for all images */
+            object-fit: cover; /* Ensures images cover the area without distortion */
         }
     </style>
     """
@@ -26,7 +28,7 @@ def run():
         This website, as the final phase of the capstone project, will contain all deliverables of the project and serve as a publicly accessible site for displaying maps.
         
         In this web application, based on the Streamlit framework, the team developed an interactive dashboard to visualize CDW flows in a variety of graphical views. This tool is useful in policy development for CDW recycling and reuse, providing insight into annual flow trends by material type, transaction, and destination, potentially facilitating a more sustainable approach to CDW management.
-        ### How to use this web?    
+        ### How to use this web?
         """)
     
     st.markdown("""<iframe width="800" height="450" src="https://cdn.pixabay.com/video/2016/12/31/6962-197634410_large.mp4" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
@@ -57,12 +59,9 @@ def run():
     df['linkedin'] = df.apply(lambda x: make_clickable(x['linkedin'], 'LinkedIn'), axis=1)
     df['email'] = df.apply(lambda x: make_clickable(f"mailto:{x['email']}", x['email']), axis=1)
     
-    # Selecting columns and adjusting the order
-    df = df[['name', 'role', 'email', 'linkedin']]
-
     # Displaying the DataFrame using Markdown with HTML rendering to show links
     st.title("Connect with us")
-    st.write("If you have any problem, please connect with us!")
+    st.write("If youhave any problems, please connect with us!")
     st.markdown(df.to_html(escape=False, index=False), unsafe_allow_html=True)
 
 if __name__ == "__main__":
