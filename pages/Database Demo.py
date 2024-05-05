97c45ebd1d819ab65652ce178eb7b20f0012b34b
bronzekillervan <126722429+bronzekillervan@users.noreply.github.com>
2024-05-04 21:46:03 -0400

    Update Database Demo.py

--- pages/Database Demo.py
+++ pages/Database Demo.py
@@ -9,7 +9,9 @@ def load_data_from_github(url):
 github_raw_url = 'https://raw.githubusercontent.com/TianyiWuNYU/test/main/data/cdw_csv_processed.csv'
 data = load_data_from_github(github_raw_url)
 
-# Prepare the table with index
+# Ensure generator_name is a string and handle NaN values
+data['generator_name'] = data['generator_name'].fillna('').astype(str)
+
 small_table = data[['generator_name']].copy()
 small_table.reset_index(inplace=True)
 
@@ -18,7 +20,7 @@ search_query = st.text_input("Search by generator name:")
 
 # Filter data based on search input
 if search_query:
-    filtered_table = small_table[small_table['generator_name'].str.contains(search_query, case=False)]
+    filtered_table = small_table[small_table['generator_name'].str.contains(search_query, case=False, na=False)]
 else:
     filtered_table = small_table
 
