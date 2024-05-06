2f18b7c02825ce12ed4fcbe196cccd99fd02e450
bronzekillervan <126722429+bronzekillervan@users.noreply.github.com>
2024-05-06 08:18:39 -0400

    Update File Upload.py

--- pages/File Upload.py
+++ pages/File Upload.py
@@ -50,6 +50,7 @@ if 'gene_zip' not in st.session_state:
     st.session_state.gene_zip = ""
 
 
+
 pickup_book = load_pickup_book()
 pickup_book['display'] = pickup_book['name'] + ' - ' + pickup_book['address']
 
@@ -103,12 +104,18 @@ with st.form("my_form"):
     gn_state = st.text_input("State", value=st.session_state.gene_state, key="gns")
     gn_zip = st.text_input("Zip", value=st.session_state.gene_zip, key="gnz")
     
+    st.write("#### Waste Info")
+    waste_quantity = st.text_input("Waste Quantity", value='', key='waste_quantity')
+    waste_type = st.text_input("Type of Debris", value='', key='waste_type')
+    trans_name = st.text_input("Transporter Name", value='', key='trans_name')
+
+    
     submitted = st.form_submit_button("Submit")
 
 if submitted:
     with open('data/template.csv', 'a', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
-        writer.writerow([pk_name, pk_add, pk_city, pk_state, pk_zip, pk_lat, pk_lng, gn_name, gn_add, gn_city, gn_state, gn_zip])
+        writer.writerow([waste_type,waste_quantity,pk_name, pk_add, pk_city, pk_state, pk_zip,trans_name, pk_lat, pk_lng, gn_name, gn_add, gn_city, gn_state, gn_zip])
         st.success("Submit Success")
 
 st.write("### Input Results", load_data())
